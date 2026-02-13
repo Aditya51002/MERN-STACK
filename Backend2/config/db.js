53c2698d914220288780110746ee707e83883620
Aditya51002 <adityamaurya510@gmail.com>
2026-02-13 11:24:35 +0530

    Added the backend file

--- Backend2/config/db.js
+++ Backend2/config/db.js
@@ -1,3 +1,9 @@
 const mongoose=require('mogoose')
 
-const
+const connectDB=()=>{
+    try{
+        mongoose.connect("mongodb+srv://useraditya:aditya510@@adityadb1practice.xg5moow.mongodb.net/?appName=AdityaDb1practice")
+    }catch(err){
+        
+    }
+}
